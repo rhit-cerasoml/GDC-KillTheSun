95e53717e22ccf0daad23074f99f6f7a68555324
rhit-cerasoml <cerasoml@rose-hulman.edu>
2024-10-15 20:04:24 -0400

    broken look script

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -8,24 +8,29 @@ public partial class PlayerMovement : RigidBody3D {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
 		Input.MouseMode = Input.MouseModeEnum.Captured;
-		
 	}
 	
 	public override void _Input(InputEvent @event){
 		if (@event is InputEventMouseMotion motionEvent){
 			lookDirection -= motionEvent.Relative / 1000.0f;
-
-			Transform3D transform = Transform;
-			transform.Basis = Basis.Identity;
-			Transform = transform;
-
-			RotateObjectLocal(Vector3.Up, lookDirection[0]); 
-			RotateObjectLocal(Vector3.Right, lookDirection[1]);
 		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta) {
+		Camera3D camera = GetNodeOrNull<Camera3D>("PlayerCam");
 		
+		Transform3D cameraTransform = camera.Transform;
+		cameraTransform.Basis = Basis.Identity;
+		camera.Transform = cameraTransform;
+
+		camera.RotateObjectLocal(Vector3.Right, lookDirection[1]);
+	}
+
+	public override void _IntegrateForces(PhysicsDirectBodyState3D state)
+	{
+		Transform3D transform = state.Transform;
+		transform.Basis = Basis.Identity;
+		state.Transform = transform.RotatedLocal(Vector3.Up, lookDirection[0]);
 	}
 }
